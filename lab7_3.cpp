1480c3e534a3e8ae5dbdb2f8be7f153a69b2d194
charnchol <khumball46@gmail.com>
2021-11-30 16:44:05 +0700

    lab 7-3 complete

--- lab7_3.cpp
+++ lab7_3.cpp
@@ -1,7 +1,28 @@
 #include<iostream>
-
+#include<cmath>
 using namespace std;
 
+int adiff(int A,int B){
+  int C;
+  while(A > 360){
+    A -= 360;
+  }
+  while(B > 360){
+    B -= 360;
+  }
+  if(A > B){
+    C = A - B;
+  }else{
+    C = B - A;
+  }
+  while(C >= 360){
+    C -= 360;
+  }
+  if(C>180){
+    C = 360 - C;
+  }
+  return C ;
+}
 
 int main(){
   cout << adiff(180,270);
